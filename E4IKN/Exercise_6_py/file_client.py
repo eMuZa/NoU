b91eef28075c60adbfaa76d0ae93b237df12b0ff
Mikkel Jensen <MikkelJensen199726@hotmail.com>
2019-02-21 21:01:56 +0100

    jaaaa

--- E4IKN/Exercise_6_py/file_client.py
+++ E4IKN/Exercise_6_py/file_client.py
@@ -2,14 +2,15 @@ import sys
 from socket import *
 from lib import Lib
 
+import socket
+
 PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
-	serverName = 'servername'
-    serverPort = PORT
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName.serverPort))
+    serverName = '127.0.0.1'
+    clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    clientSocket.connect((serverName, PORT))
     sentence = raw_input('Input lowercase sentence:')
     clientSocket.send(sentence.encode())
     modifiedSentence = clientSocket.recv(1024)
@@ -18,6 +19,7 @@ def main(argv):
     clientSocket.close()
 def receiveFile(fileName,  conn):
 	# TO DO Your Code
+	print('jajaja')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
