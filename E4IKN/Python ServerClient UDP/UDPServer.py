3d3dad4972d8f9dd9c6bf171133d1e3e9e2b6294
Mikkel <MikkelJensen199726@hotmail.com>
2019-03-14 11:44:15 +0100

    UDP ServerClient

--- E4IKN/Python ServerClient UDP/UDPServer.py
+++ E4IKN/Python ServerClient UDP/UDPServer.py
@@ -2,6 +2,9 @@ import sys
 import socket
 import os
 
+fileName = "/proc/uptime"
+fileName2 = "/proc/loadavg"
+
 HOST = '10.0.0.2'
 PORT = 9000
 BUFSIZE = 1024
@@ -22,14 +25,22 @@ def main(argv):
         message = bytesAddrPair[0]
         addr = bytesAddrPair[1]
 
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
+        clientMsg = message
+        clientIP  = "Client IP Address:{}".format(addr)
 
         print(clientMsg)
         print(clientIP)
 
+        if ((message == "u") or (message == "U")):
+
+            with open(fileName, 'rb') as f:
+                bytestoSend = f.read(BUFSIZE)
+        else:
+            with open(fileName2, 'rb') as t:
+                bytestoSend = t.read(BUFSIZE)
+
     #Sending to client
-        UDPServerSocket.sendto(clientMsg,addr)
+        UDPServerSocket.sendto(bytestoSend,addr)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
